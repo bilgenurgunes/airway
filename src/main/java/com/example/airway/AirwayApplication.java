b7feddf6517c01bf15b9aee810da14166f73fecd
bnrgns2022 <gunesbilgenur3577@gmail.com>
2022-08-25 15:42:19 +0300

    Initial commit

---
+++ src/main/java/com/example/airway/AirwayApplication.java
@@ -0,0 +1,13 @@
+package com.example.airway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/airway/AirwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
